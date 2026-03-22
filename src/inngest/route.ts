6fc31c2fb5b028d17887220d155dde080daa6fe5
Spandana N S <spandu-282003>
2026-03-22 13:51:41 +0530

    13: ai agent & tools

--- src/inngest/route.ts
+++ src/inngest/route.ts
@@ -1,10 +1,14 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { helloWorld } from "@/app/api/inngest/functions";
+import { demoError, demoGenerate } from "@/app/api/inngest/functions";
+import { processMessage } from "@/features/conversations/inngest/process-message";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld, // <-- This is where you'll always add all your functions
+    demoGenerate,
+    demoError,
+    processMessage,
   ],
 });
+
