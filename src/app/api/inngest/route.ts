3a311e792b5a7346979988e0d5e3ef5f5b088449
Spandana N S <spandu-282003>
2026-03-23 19:21:32 +0530

    15: github import and export

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,15 +1,16 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/inngest/client";
-import { demoGenerate, demoError } from "@/app/api/inngest/functions";
 import { processMessage } from "@/features/conversations/inngest/process-message";
+import { importGithubRepo } from "@/features/auth/components/projects/inngest/import-github-repo";
+import { exportToGithub } from "@/features/auth/components/projects/inngest/export-to-github";
 
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    demoGenerate,
-    demoError,
     processMessage,
+    importGithubRepo,
+    exportToGithub,
   ],
 });
-
